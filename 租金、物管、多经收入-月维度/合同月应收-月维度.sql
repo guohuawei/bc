160faa8f73d131b78b1ed8e6647afdc39adbf386
guohuawei <ghwwyk@163.com>
2022-02-23 14:07:15 +0800

    update

--- 租金、物管、多经收入-月维度/合同月应收-月维度.sql
+++ 租金、物管、多经收入-月维度/合同月应收-月维度.sql
@@ -1,20 +1,20 @@
--- 租金、物管 收入（权责口径）
-select t1.project_id,                  -- 项目id
-       bas_project.project_name,       -- 项目名称
-       bas_project.project_type,       -- 经营类型(1001:购物中心 1002:商业街 1003:写字楼 1004：轻资产)
-       bas_project.project_name_short, -- 项目简称
-       bas_project.region_id,          -- 区域id
-       bas_region.region_name,         -- 区域名称
-       t1.fee_type,                    -- 费项类型(1001:租金 1002:物管 1003:保证金 1004:滞纳金 1005:能耗 1006:暂收款 1007:多经)
-       t1.receivable_date,             -- 应收月
-       t1.receivable_amount,           -- 应收金额
-       t1.sub_amount,                  -- 减免金额
-       t1.tenant_name,                 -- 商家名称
-       t1.tenant_id,                   -- 商家id
-       t1.cont_id,                     -- 合同id
-       t1.cont_no,                     -- 合同号
-       t1.id,                          -- 应收id
-       current_date                    -- ETL时间
+-- 租金、物管 收入（合同口径）
+select t1.project_id,                                                                                -- 项目id
+       bas_project.project_name,                                                                     -- 项目名称
+       bas_project.project_type,                                                                     -- 经营类型(1001:购物中心 1002:商业街 1003:写字楼 1004：轻资产)
+       bas_project.project_name_short,                                                               -- 项目简称
+       bas_project.region_id,                                                                        -- 区域id
+       bas_region.region_name,                                                                       -- 区域名称
+       t1.fee_type,                                                                                  -- 费项类型(1001:租金 1002:物管 1003:保证金 1004:滞纳金 1005:能耗 1006:暂收款 1007:多经)
+       if(t1.receivable_date is null, t1.receivable_date, concat_ws('-', t1.receivable_date, '01')), -- 应收月
+       t1.receivable_amount,                                                                         -- 应收金额
+       t1.sub_amount,                                                                                -- 减免金额
+       t1.tenant_name,                                                                               -- 商家名称
+       t1.tenant_id,                                                                                 -- 商家id
+       t1.cont_id,                                                                                   -- 合同id
+       t1.cont_no,                                                                                   -- 合同号
+       t1.id,                                                                                        -- 应收id
+       current_date                                                                                  -- ETL时间
 from (
          select fin_receivable.project_id,                                      -- 项目id
                 fin_basic_fee.fee_type,                                         -- 费项类型(1001:租金 1002:物管 1003:保证金 1004:滞纳金 1005:能耗 1006:暂收款 1007:多经)
@@ -35,7 +35,8 @@ from (
                   left join ods.ods_bc_yw_pms_business_db_fin_received_ver_dt fin_received_ver
                             on fin_received_ver_item.received_ver_id = fin_received_ver.id and
                                fin_received_ver.is_deleted = '0'
-         where fin_receivable.is_deleted = '0' and fin_receivable.is_to_push = '1001'
+         where fin_receivable.is_deleted = '0'
+           and fin_receivable.is_to_push = '1001'
          group by fin_receivable.id,
                   fin_receivable.project_id,
                   fin_receivable.tenant_id,
